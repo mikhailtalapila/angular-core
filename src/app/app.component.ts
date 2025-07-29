f3725bc046c5eb8e41a9ec3c144ced21aac4e6d5
Michael Talapila <mikhailtalapila@yahoo.com>
2025-07-29 19:37:23 -0400

    Viewchild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,40 @@
-import { Component } from '@angular/core';
-import {COURSES} from '../db-data';
-import { Course } from './model/course';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from "@angular/core";
+import { COURSES } from "../db-data";
+import { Course } from "./model/course";
+import { CourseCardComponent } from "./course-card/course-card.component";
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    standalone: false
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+  standalone: false,
 })
-export class AppComponent {
-  courses = [...COURSES];
-
-  trackCourse(index: number, course: Course) {
-    return course.id;
+export class AppComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    this.cards.changes.subscribe((cards) => console.log(cards));
   }
+  courses = [...COURSES];
+  @ViewChildren(CourseCardComponent, {read: ElementRef})
+  cards: QueryList<ElementRef>;
 
+  onCourseSelected(course: Course) {}
+  onCoursesEdited() {
+    this.courses.push({
+      id: 2,
+      description: "RxJs In Practice Course",
+      iconUrl:
+        "https://s3-us-west-1.amazonaws.com/angular-university/course-images/rxjs-in-practice-course.png",
+      longDescription:
+        "Understand the RxJs Observable pattern, learn the RxJs Operators via practical examples",
+      category: "BEGINNER",
+      lessonsCount: 10,
+    });
+  }
 }
--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Course } from '../model/course';
 import { COURSES } from 'src/db-data';
 import { CommonModule, NgIf } from '@angular/common';
+
 @Component({
   selector: 'course-card',
   imports: [
@@ -13,21 +14,11 @@ import { CommonModule, NgIf } from '@angular/common';
 export class CourseCardComponent {
   @Input()
   course: Course;
-  @Input({
-    required: true
-  })
-  index: number;
-  isImageAvailable() {
-    return this.course && this.course.iconUrl
-  }
-  cardClasses() {
-    if (this.course.category === 'BEGINNER') {
-      return 'beginner'
-    }
-  }
-  cardStyles() {
-    return {
-      'background-image': 'url(' + this.course.iconUrl + ')'
-    }
+
+  @Output('courseSelected')
+  courseEmitter = new EventEmitter<Course>();
+
+  onCourseViewed() {
+    this.courseEmitter.emit(this.course);
   }
 }
